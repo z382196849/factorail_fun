862d1246bc5e11fac232dea44bea7bfe697e4860
z382196829 <1936851315@qq.com>
2021-12-09 23:56:05 +0800

    第二十次提交

---
+++ test.c
@@ -0,0 +1,26 @@
+﻿#include<stdio.h>
+#include<windows.h>
+
+int fac(int n)
+{
+    if(n < 2)
+    {
+	return 1;
+    }
+    else
+    {
+	return n * fac(n - 1);
+    }
+}
+
+int main()
+{
+    int i = 0;
+    int ret = 0;
+    printf("请输入：i = ");
+    scanf("%d", &i);
+    ret = fac(i);
+    printf("fac(%d) = %d\n", i, ret);
+    system("pause");
+    return;
+}
